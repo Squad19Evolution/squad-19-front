c36d7998000a5af1bb569b9cd6cafc1ab34ab337
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-11-06 10:11:57 -0300

    chore: set up router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import DashBoard from './pages/Dashboard';
+import Landing from './pages/Landing';
+import Login from './pages/Login';
+
 function App() {
-  return <div className='App'>App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/dashboard' element={<DashBoard />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/Login.tsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+
+function Login() {
+  return (
+    <div className='Login'>
+      <input type='text' placeholder='login' />
+      <input type='password' name='' id='' placeholder='senha' />
+      <Link to='/dashboard'>
+        <button>Login</button>
+      </Link>
+    </div>
+  );
+}
+
+export default Login;
