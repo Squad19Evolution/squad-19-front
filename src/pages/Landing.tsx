061ae0eb95615b3f46df1e2838c751452e79dc3f
JeanVictorL <jeanvictor.lopes@gmail.com>
2022-11-11 23:24:47 -0300

    feat: mobile landing page finished

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -3,12 +3,20 @@ import HeaderTwo from '../components/HeaderTwo';
 
 function Landing() {
   return (
-    <div className='text-black '>
+    <div className='  '>
       <HeaderTwo />
-      <h1>landing</h1>
-      <Link to='/login'>
-        <button>Login</button>
-      </Link>
+      <div className=' mx-auto flex flex-col items-center gap-y-6 mt-36 sm:mt-10 mb-10 ' >
+        <img src="/src/media/studying 1.svg" alt="books" />
+        <h1 className=' text-orangeJuice text-center w-80 text-2xl font-bold ' >
+          Venha vitaminar o seu conhecimento com a gente!
+        </h1>
+        <p className=' text-orangeJuice text-center w-80 text-base leading-5 ' >
+          Estude através de trilhas pré-definidas
+          por mentores da FCamara que são
+          especialistas em tecnologia!
+        </p>
+        <button className=' bg-azulFormacao w-40 h-10 ' >Cadastre-se</button>
+      </div>
     </div>
   );
 }
