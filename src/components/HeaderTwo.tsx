29369d082ba02240e11f6deba2f1ba7b6e2597d7
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-11-12 03:24:28 -0300

    feat: add static files and content-item component

--- src/components/HeaderTwo.tsx
+++ src/components/HeaderTwo.tsx
@@ -3,7 +3,7 @@ import LoginButtom from "./LoginButtom";
 
 function HeaderTwo() {
   return (
-    <nav className='bg-[#C4D3FD] border-gray-200 w-full '>
+    <nav className='bg-[#C4D3FD] border-gray-200 w-full'>
       <div className='flex flex-wrap w-full '>
         <div className="bg-azulFormacao md:w-48 w-20 h-7 md:h-14 rounded-lg my-5 md:my-6 ml-7 md:ml-28 " ></div>
         <Link to="/" className=" ml-auto text-azulFormacao text-xs md:text-base mr-4 md:mr-12 mt-7 md:mt-10 font-bold " >Home</Link>
