6b0cfc5676dfd01209a43e2b45c969eedcedca23
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-11-15 17:38:56 -0300

    feat: add progress functionality

--- src/components/Progress.tsx
+++ src/components/Progress.tsx
@@ -1,7 +1,15 @@
+import { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalContext';
 import ProgressBar from './ProgressBar';
 
-function Progress() {
-  let percent = 90;
+function Progress(props) {
+  const { completedContents } = useContext(GlobalContext);
+  const completedPathContents = completedContents.filter(
+    (el) => el.path_id === props.path.id,
+  );
+  const percent = Math.floor(
+    (completedPathContents.length / props.path.contents.length) * 100,
+  );
 
   return (
     <div className=' bg-[#C4D3FD] flex flex-col gap-y-2 md:gap-y-4 text-azulFormacao rounded-lg p-3 my-4 w-[100%] '>
@@ -13,7 +21,9 @@ function Progress() {
         <span className=' font-semibold md:text-2xl '>
           Tempo para conclusão:
         </span>
-        <span className=' font-bold text-xs md:text-base '>1h20</span>
+        <span className=' font-bold text-xs md:text-base '>
+          {props.duration}
+        </span>
       </div>
       <div className=' flex flex-col gap-y-0 '>
         <span className=' font-semibold md:text-2xl '>Atualizado em:</span>
